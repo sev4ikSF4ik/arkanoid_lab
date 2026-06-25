d5e7b262aedd3d528988e4562e366b22772f6baa
sev4ikSF4ik <dmuck2755@gmail.com>
2026-06-25 17:32:42 +0300

    Add argparse for custom settings

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import pygame
 import argparse
 import sys
 
+parser = argparse.ArgumentParser(description='Arkanoid Game')
+parser.add_argument('--speed', type=int, default=5, help='Скорость мяча')
+parser.add_argument('--bg', type=str, choices=['black', 'white'], default='black', help='Цвет фона')
+args = parser.parse_args()
+
