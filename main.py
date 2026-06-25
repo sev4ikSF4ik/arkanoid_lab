eebad8339921690632984bc6cc5163bff82568cc
sev4ikSF4ik <dmuck2755@gmail.com>
2026-06-25 17:32:47 +0300

    Initialize game objects and bricks

--- main.py
+++ main.py
@@ -43,3 +43,7 @@ class Ball:
         if self.rect.top < 0:
             self.dy *= -1
 
+paddle = Paddle()
+ball = Ball(args.speed)
+bricks = [pygame.Rect(15 + i * 78, 20 + j * 35, 70, 25) for i in range(10) for j in range(5)]
+
